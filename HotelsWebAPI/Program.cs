e32c3adb4518af8eca557cef32341ede635808d4
Pavlo Pavlichenko <pavlo.pavlichenko@symphony-solutions.com>
2022-10-21 13:10:02 +0300

    HotelsWebAPI added

---
+++ HotelsWebAPI/Program.cs
@@ -0,0 +1,58 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelDb>(options =>
+{
+  var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+  options.UseNpgsql(connectionString!);
+});
+
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+
+builder.Services.AddCors(p => 
+  p.AddDefaultPolicy(
+    builder =>
+    {
+        builder
+          .AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+app.UseCors();
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+db.Database.EnsureCreated();
+
+app.MapGet("/hotels", async (IHotelRepository repository) => 
+  Results.Ok(await repository.GetHotelsAsync()));
+
+app.MapGet("/hotels/{id}", async (int id, IHotelRepository repository) => 
+  await repository.GetHotelAsync(id) is Hotel hotel
+  ? Results.Ok(hotel)
+  : Results.NotFound());
+
+app.MapPost("/hotels", async ([FromBody]Hotel hotel, IHotelRepository repository) => 
+{
+  await repository.InsertHotelAsync(hotel);
+  await repository.SaveAsync();
+  return Results.Created($"/hotels/{hotel.Id}", hotel.Id);
+});
+
+app.MapPut("/hotels", async ([FromBody] Hotel hotel, IHotelRepository repository) => 
+{
+  await repository.UpdateHotelAsync(hotel);
+  await repository.SaveAsync();
+  return Results.NoContent();
+});
+
+app.MapDelete("/hotels/{id}", async (int id, IHotelRepository repository) => 
+{
+  await repository.DeleteHotelAsync(id);
+  await repository.SaveAsync();
+  return Results.NoContent();
+});
+
+app.Run();
