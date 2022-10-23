c9d1b98dc7eaba8d5f03240fac7a2f0d0e2d46bc
Pavlo Pavlichenko <pavlo.pavlichenko@symphony-solutions.com>
2022-10-23 11:27:57 +0300

    Fixed wrong namings and added postgres settings

--- MigrationInit/Data/Hotel.cs
+++
@@ -1,7 +0,0 @@
-public class Hotel
-{
-  public int Id { get; set; }
-  public string Name { get; set; } = string.Empty;
-  public double Latitude { get; set; }
-  public double Longitude { get; set; }
-}
